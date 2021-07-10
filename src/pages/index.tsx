322f8dea1620066eb2a8053afb3d0cd49780777c
Rajas Paranjpe <paranjperajas@gmail.com>
2021-07-10 10:57:12 -0500

    Title with React Helmet

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,14 @@
 import * as React from 'react'
 import { FC } from 'react'
+import Helmet from 'react-helmet'
 
-const IndexPage: FC = () => <h1>Index Page Content</h1>
+const IndexPage: FC = () => (
+  <>
+    <Helmet>
+      <title>Games</title>
+    </Helmet>
+    <h1>Index Page Content</h1>
+  </>
+)
 
 export default IndexPage
